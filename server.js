1f1ed267fa74a69fc18e85d50a0da8e7c3754cf6
wfogame <aydenhliu@gmail.com>
2025-06-07 23:42:34 -0400

    Made a SOrry, can't find that using express

--- server.js
+++ server.js
@@ -21,4 +21,26 @@ app.listen(port, () => {
 
 
 
-    
+app.use((req, res, next) => {
+res.setHeader('X-Content-Type-Options', 'nosniff');
+  res.setHeader('X-Frame-Options', 'DENY');
+  res.setHeader('X-XSS-Protection', '1; mode=block');
+  res.setHeader('Referrer-Policy', 'no-referrer');
+  res.setHeader('Permissions-Policy', 'geolocation=(), microphone=()');
+  res.setHeader('Strict-Transport-Security', 'max-age=31536000; includeSubDomains');
+  res.setHeader('Content-Type',"text/html");
+    res.status(404).send(`
+    <!DOCTYPE html>
+    <html>
+      <head><title>404 Not Found</title>
+      
+      <link rel = 'stylesheet' href = '/style.css'>
+      </head>
+      <body>
+        <h1>Sorry, can't find that!</h1>
+        <p>The page you requested does not exist.</p>
+      </body>
+    </html>
+  `);
+  
+})
